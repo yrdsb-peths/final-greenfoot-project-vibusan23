d72b99ee9b421aa1e4143b536d2287d7e2924e29
vibusan23 <340876986@gapps.yrdsb.ca>
2023-01-10 14:47:11 -0500

    Man finished

--- Man.java
+++ Man.java
@@ -12,8 +12,28 @@ public class Man extends Actor
      * Act - do whatever the Man wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public Man()
+    {
+        GreenfootImage image = getImage();
+        image.scale(150,150);
+        setImage(image);
+    }
+    
     public void act()
     {
-        // Add your action code here.
+        int x = getX();
+        int y = getY();
+        
+        if(Greenfoot.isKeyDown("up"))
+        {
+            y -= 2;
+        }
+        
+        if(Greenfoot.isKeyDown("down"))
+        {
+            y += 2;
+        }
+        setLocation(x, y);
+        
     }
 }
---
+++ Zombie.java
@@ -0,0 +1,35 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.World;
+
+/**
+ * Write a description of class Zombie here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Zombie extends Actor
+{
+    /**
+     * Act - do whatever the Zombie wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */    
+    int speed = 1;
+    public void act()
+    {        
+        int x = getX();
+        int y = getY() + speed;
+        setLocation(x, y);
+        
+        MyWorld world = (MyWorld) getWorld();
+        if (getX() >= world.getWidth())
+        {
+            world.gameOver();
+            world.removeObject(this);
+        }
+    }
+    
+    public void setSpeed(int spd)
+    {
+        speed = spd;
+    }
+}
